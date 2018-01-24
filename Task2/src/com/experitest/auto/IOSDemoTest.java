74f37b81b93dfe848c64b0008a0658ab70d0c357
Alokbhagat <31006030+Alokbhagat@users.noreply.github.com>
2018-01-24 14:35:26 +0530

    for demo changed user name and password

--- Task2/src/com/experitest/auto/IOSDemoTest.java
+++ Task2/src/com/experitest/auto/IOSDemoTest.java
@@ -2,98 +2,77 @@ package com.experitest.auto;
 
 import java.net.URL;
 import java.util.List;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
-import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
-import io.appium.java_client.remote.IOSMobileCapabilityType;
 import io.appium.java_client.remote.MobileBrowserType;
-import io.appium.java_client.remote.MobileCapabilityType;
-
-import com.experitest.manager.api.ManagerPublisher;
-import com.experitest.manager.client.PManager;
-import com.experitest.manager.testng.ManagerTestNGListener;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
- 
-import java.io.File;
+import org.openqa.selenium.support.ui.WebDriverWait; 
 import java.lang.reflect.Method;
  
 
+//Ios test cases 
 public class IOSDemoTest extends BaseTest {
+	//create the driver 
 	protected IOSDriver<IOSElement> driver = null;
-
 	@BeforeMethod
 	@Parameters("iosdeviceQuery")
 	public void setUp(@Optional("@os='ios'") String deviceQuery,Method method) throws Exception {
-		init(deviceQuery);
-		
-		// Init application / device capabilities
-		//dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
-		//dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
-		dc.setCapability("testName", method.getName());
-	
+		init(deviceQuery);		
+		//setting up the desired capabilities - you can set more properties in side this 
+		dc.setCapability("testName", method.getName());	
 		dc.setCapability("project", getProperty("project",cloudProperties));
 		dc.setBrowserName(MobileBrowserType.SAFARI);
-		driver = new IOSDriver<>(new URL(getProperty("url",cloudProperties) + "/wd/hub"), dc);
-		System.out.println("runing : " + method.getName());
-		System.out.println("runing : " + deviceQuery);
+		//From cloud.properties all setting are getting populated 
+		driver = new IOSDriver<>(new URL(getProperty("url",cloudProperties) + "/wd/hub"), dc);	
 	}
 
+	//Test case to get customer list from experitest.com
 	@Test
-	public void getCustomersTest() {
-		
+	public void getCustomersTest() {		
+		int i = 0;		
+		String companyName;
         driver.get("http://experitest.com/customers/");		
         driver.context("WEBVIEW_1");
-		List<IOSElement> customers = driver.findElements(By.xpath("//*[@id='div']/img"));	       
-		System.out.println("Total Number of  Customers = " + customers.size());
-		String companyName;
-	      int i = 0;
-		
+        //getting all logo web element in side the list 
+		List<IOSElement> customers = driver.findElements(By.xpath("//*[@id='div']/img"));      
+		//reading the web element info from list 
 		for (WebElement webElement : customers) {
 			i++;
-			 if(i > 10){
-				
+			// Since there are lot of customers so restricted to print only 10 customers
+			 if(i > 10){				
 				break;
 			}
+			 // spliting up the string img src to short names
 		     companyName = webElement.getAttribute("src");		     
 		     String[] words=companyName.split("/");		     
 			System.out.println("Customer Name:  " + words[6]);
-		}
-		
+			}	
 	}
 	
+	//Test case to open experitest and click on online guide 
 	@Test
-	public void webLoginApplicationTest() {
-		
-		  driver.get("experitest.com");
-		  
+	public void webLoginApplicationTest() {		
+		  driver.get("http://experitest.com");
 		  driver.context("WEBVIEW_1");
 		  try{Thread.sleep(4000);} catch(Exception ignore){}
-
-		  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@nodeName='BUTTON' and @css='BUTTON.navbar-toggle']")));
+		  new WebDriverWait(driver, 100).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@nodeName='BUTTON' and @css='BUTTON.navbar-toggle']")));
 		  driver.findElement(By.xpath("//*[@nodeName='BUTTON' and @css='BUTTON.navbar-toggle']")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Capabilities']")));
+		  new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Capabilities']")));
 		  driver.findElement(By.xpath("//*[@text='Support' and @css=concat('A[href=', \"'\", '#', \"'\", ']')]")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Online Guide']")));
+		  new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Online Guide']")));
 		  driver.findElement(By.xpath("//*[@text='Online Guide' and @css=concat('A[href=', \"'\", '#', \"'\", ']')]")).click();
 
-
 	}
-
+	//this is teardown - once execution is done - driver will be released - you can add other code in this to clean the enviornment
 	@AfterMethod
-	public void tearDown() {
-	
+	public void tearDown() {	
 		driver.quit();
 	}
 
--- Task2/src/com/experitest/auto/androidDemoTest2.java
+++ Task2/src/com/experitest/auto/androidDemoTest2.java
@@ -1,96 +1,73 @@
 package com.experitest.auto;
 
 import java.net.URL;
-import java.util.List;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
-
-import com.experitest.manager.api.ManagerPublisher;
-import com.experitest.manager.client.PManager;
-import com.experitest.manager.testng.ManagerTestNGListener;
-import org.testng.annotations.*;
- 
-import java.io.File;
 import java.lang.reflect.Method;
  
 
+
+// for android test cases 
 public class androidDemoTest2 extends BaseTest {
 	
 	protected AndroidDriver<AndroidElement> driver = null;
-	
+	// this parameters are passed from testNg xml 
 	@BeforeMethod
 	@Parameters("deviceQuery")
 	public void setUp(@Optional("@os='android'") String deviceQuery,Method method) throws Exception{
 		init(deviceQuery);
+		
+		// this are test specific capabilities
 		dc.setCapability(MobileCapabilityType.NO_RESET, true);
 		dc.setCapability("instrumentApp", true);
 		dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank/.LoginActivity");
 		dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
 		dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
 		dc.setCapability(MobileCapabilityType.ORIENTATION, "PORTRAIT");
+		// for reporter setting up the test names etc
 		dc.setCapability("testName", method.getName());
 		dc.setCapability("project", getProperty("project",cloudProperties));
 		driver = new AndroidDriver<>(new URL(getProperty("url",cloudProperties) + "/wd/hub"), dc);
-		System.out.println(driver.isAppInstalled("com.experitest.ExperiBank"));
-		if(driver.isAppInstalled("com.experitest.ExperiBank") != true){
-			
+		// just making sure app is installed on the device 
+		if(driver.isAppInstalled("com.experitest.ExperiBank") != true){			
 			driver.installApp("cloud:com.experitest.ExperiBank/.LoginActivity");
-		}
-		
-	}
+			}		
+	}	
 	
-	
-	// test comment 
+	// Test case to launch the application and login into application
 	@Test
-	public void eribankTest(){			 
-		
-		
-		
+	public void launchApplicationTest(){
 		  driver.context("NATIVE_APP");
 		  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='usernameTextField']")));
-	 
-		  driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+	 	  driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
 		  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='passwordTextField']")));
-			 
 		  driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
 		  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='loginButton']")));
-		  driver.findElement(By.xpath("//*[@id='loginButton']")).click();
-		  
-		  
-	 }
+	      driver.findElement(By.xpath("//*[@id='loginButton']")).click();		  
+	 }	
 	
-	
+	//test case to login into application - making payments 
 	@Test
-	public void eribankPaymentTest(){	
-		 //driver.startActivity("cloud:com.experitest.ExperiBank", ".LoginActivity");
-		
+	public void loginApplicationTest(){	
 		  driver.context("NATIVE_APP");
 		  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='usernameTextField']")));
 	 	  driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
 	 	  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='passwordTextField']")));
 		  driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
 		  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='loginButton']")));
-		  driver.findElement(By.xpath("//*[@id='loginButton']")).click();
-		  
+		  driver.findElement(By.xpath("//*[@id='loginButton']")).click();		  
 		  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='makePaymentButton']")));
-		  driver.findElement(By.xpath("//*[@text='Make Payment']")).click();
-		  
+		  driver.findElement(By.xpath("//*[@text='Make Payment']")).click();		  
 		  new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='phoneTextField']")));
 		  driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("9923123450");
 		  driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Alok");
@@ -103,79 +80,12 @@ public class androidDemoTest2 extends BaseTest {
 		  driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
 		  driver.findElement(By.xpath("//*[@id='button1' and @text='Yes']")).click();
 		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='logoutButton']")));
-		  driver.findElement(By.xpath("//*[@id='logoutButton']")).click();
-
-		  
-	 }	
-	
-	/*@Test
-	public void MakePayMentTest(){	
-		 		
-
-		  driver.context("NATIVE_APP");	
-		  
-		  driver.findElement(By.xpath("//*[@text='Username']")).sendKeys("company");
-		 // new WebDriverWait(driver, 100).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='passwordTextField']")));
-		  driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
-		  driver.findElement(By.xpath("//*[@id='loginButton']")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='makePaymentButton']")));
-		  driver.findElement(By.xpath("//*[@text='Make Payment']")).click();
-		  
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='phoneTextField']")));
-		  driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("9923123450");
-		  driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Alok");
-		  driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("20");
-		  driver.findElement(By.xpath("//*[@id='countryButton']")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='rowTextView']")));
-		  int height = driver.findElement(By.xpath("//*[@id='countryList']")).getSize().getHeight();
-		  driver.findElement(By.xpath("//*[@id='countryList']//*[4]")).click();
- 		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='sendPaymentButton']")));
-		  driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
-		  driver.findElement(By.xpath("//*[@id='button1' and @text='Yes']")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='logoutButton']")));
-		  driver.findElement(By.xpath("//*[@id='logoutButton']")).click();
-
-	 }
-	
-	@Test
-	public void  WebApplicationTest(){	
-		 //driver.startActivity("cloud:com.experitest.ExperiBank", ".LoginActivity");
-	
-
-		 driver.get("http://experitest.com");
-		 driver.context("WEBVIEW_1");
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@nodeName='BUTTON' and @css='BUTTON.navbar-toggle']")));
-		  driver.findElement(By.xpath("//*[@nodeName='BUTTON' and @css='BUTTON.navbar-toggle']")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Support']")));
-		  driver.findElement(By.xpath("//*[@text='Support']")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Online Guide']")));
-		  driver.findElement(By.xpath("//*[@text='Online Guide']")).click();
-		  driver.pressKeyCode(AndroidKeyCode.HOME);
-		  
-	 }		  
-	
-	
-	
-	@Test
-	public void  WebApplication2Test(){	
-		 //driver.startActivity("cloud:com.experitest.ExperiBank", ".LoginActivity");
-	 	 driver.get("http://experitest.com");
-		 driver.context("WEBVIEW_1");
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='LOGIN']")));
-		  driver.findElement(By.xpath("//*[@text='LOGIN']")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Sign up        ']")));
-		  driver.findElement(By.xpath("//*[@text='Sign up        ']")).click();
-		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Download']")));
-		  driver.findElement(By.xpath("//*[@text='Download']")).click();
-		  driver.pressKeyCode(AndroidKeyCode.HOME);
-
-		  
-	 }*/
-
+		  driver.findElement(By.xpath("//*[@id='logoutButton']")).click();	  
+	 }			
+//this is teardown - once execution is done - driver will be released - you can add other code in this to clean the enviornment
 	@AfterMethod
 	public void tearDown(){
-		
-		driver.quit();
+			driver.quit();
 	}
 	
 	
